615f2762125ff141efb723725e79531a231bb570
Manav Sehgal <manavsehgal1128@gmail.com>
2019-11-08 09:37:38 -0500

    Update test DB URL

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
   URL: process.env.BASE_URL || 'http://localhost:3000',
   MONGODB_URI:
     process.env.MONGODB_URI ||
-    'mongodb+srv://mseh1128:manav123@cluster0-obs78.mongodb.net/test?retryWrites=true&w=majority',
+    'mongodb://manav123:J4EV8vit426BWQ@ds241258.mlab.com:41258/milestone',
   JWT_SECRET: process.env.JWT_SECRET || 'mysecret'
 };
