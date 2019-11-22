dda1683835ad7767d8d4d55526c24c1c291e42db
unknown <mseh1128@gmail.com>
2019-11-22 09:53:29 -0500

    Cleared upload files collections in reset

--- routes/api/HelperRoutes.js
+++ routes/api/HelperRoutes.js
@@ -13,7 +13,7 @@ router.post('/reset', async (req, res) => {
     await Item.remove({});
     await User.remove({});
     await Media.remove({});
-    res.locals.gfs.remove({});
+    await res.locals.gfs.collection('uploads').remove({});
     res.json({ status: 'OK' });
   } catch (err) {
     console.log(err);
